80df30aafb3996148d5d649d84e1c4ce6a182354
martin@m-code.com.ar <martin@m-code.com.ar>
2021-12-05 11:28:10 -0300

    Modificado header

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',

a4ad7d7392bbe2d72306241367ac0499e850092b
MH <martin@m-code.com.ar>
2021-12-14 21:28:57 -0300

    Casi listo sin BD 1

--- portfolio/src/app/components/proyects-card/proyects-card.component.ts
+++ portfolio/src/app/components/proyects-card/proyects-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-proyects-card',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyects-card.component.css']
 })
 export class ProyectsCardComponent implements OnInit {
-
+  iconEdit:IconDefinition =faPen
+  iconDelete: IconDefinition =  faTrash
   constructor() { }
 
   ngOnInit(): void {

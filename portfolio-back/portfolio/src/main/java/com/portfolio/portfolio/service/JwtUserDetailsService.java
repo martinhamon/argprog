344177f1440dadaa2f80e766a418e5157c46194d
MH <martin@m-code.com.ar>
2021-12-28 21:42:06 -0300

    Cambio api desde cero

--- portfolio-back/portfolio/src/main/java/com/portfolio/portfolio/service/JwtUserDetailsService.java
+++ portfolio-back/portfolio/src/main/java/com/portfolio/portfolio/service/JwtUserDetailsService.java
@@ -18,7 +18,7 @@ public class JwtUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		if ("techgeeknext".equals(username)) {
-			return new User("caradepija", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+			return new User("techgeeknext", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
 					new ArrayList<>());
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
